cba16d9ccde5c5f8faa56d8ffa5beabd507540f8
HarentsoaR <randriamaholimanana1@gmail.com>
2024-04-01 14:10:01 +0300

    Classe HUMAIN

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -1,43 +1,7 @@
 package iut.bad;
 
-public class Femme {
-    private String nom;
-    private String prenom;
-    private int age;
-
-    // Constructeur par défaut
-    public Femme() {
-    }
-
-    // Constructeur avec tous les champs
-    public Femme(String nom, String prenom, int age) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.age = age;
-    }
-
-    // Getters et Setters pour chaque champ
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+public class Femme extends Humain {
+	public Femme(String nom, String prenom, int age) {
+        super(nom, prenom, age);
     }
 }
